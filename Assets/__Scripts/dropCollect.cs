db4f6cbcf3cb16d9b1117ea78b4fd2f66cbc0887
Claire Yin <yxm0621@gmail.com>
2014-12-03 16:28:29 -0800

    add level connection and rename scenes

--- Assets/__Scripts/dropCollect.cs
+++ Assets/__Scripts/dropCollect.cs
@@ -12,6 +12,8 @@ public class dropCollect : MonoBehaviour {
 	public float gravityRangeMin = 0.05f;
 	public float gravityRangeMax = 0.3f;
 	public float timeInterval = 0.3f;
+	
+	public bool levelFinished = false;
 
 	// Use this for initialization
 	IEnumerator Start () {
@@ -22,6 +24,7 @@ public class dropCollect : MonoBehaviour {
 			Vector3 pos = new Vector3(posX, posY, 0f);
 			drops[i] = Instantiate(drop, pos, Quaternion.identity) as GameObject;
 			drops[i].rigidbody2D.gravityScale = Random.Range(gravityRangeMin, gravityRangeMax);
+			drops[i].name = "dewDrop";
 			yield return new WaitForSeconds(timeInterval);
 		}
 
@@ -29,6 +32,9 @@ public class dropCollect : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (GameObject.Find("dewDrop") == null) {
+			GameObject.Find("Clouds BG").collider2D.enabled = false;
+			levelFinished = true;
+		}
 	}
 }
---
+++ Assets/__Scripts/title/titleMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class titleMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject.Find("dropletTitle").renderer.enabled = true;
+		GameObject.Find("chapterScreen").renderer.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.timeSinceLevelLoad > 3f) {
+			GameObject.Find("dropletTitle").renderer.enabled = false;
+			GameObject.Find("chapterScreen").renderer.enabled = true;
+			if (Input.GetKey(KeyCode.Space)) {
+				Application.LoadLevel("Prologue");
+			}
+		}
+	
+	}
+}
